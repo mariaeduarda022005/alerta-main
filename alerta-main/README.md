f92a9a6b11c64f270788b125f51e2b216dbb6ca3
mariaeduarda022005 <mariaeduardaprogramadora23@gmail.com>
2024-06-10 20:48:43 -0300

    gitgnore

---
+++ alerta-main/README.md
@@ -0,0 +1,25 @@
+# App Aferição Pluvial
+
+Este projeto é uma aplicação de monitoramento que utiliza MQTT para receber dados de sensores e envia alertas via SMS usando o Twilio quando determinadas condições são atendidas.
+
+## Requisitos
+
+- Node.js (versão 18.17.0 ou superior)
+- npm (versão 8.x.x ou superior)
+
+## Configuração do Projeto
+
+1. **Clone o Repositório**
+
+   ```sh
+   git clone https://github.com/seu-usuario/alerta.git
+   cd alerta
+
+2. **Instale as Dependências**
+    ```sh
+    npm install
+
+3. **Inicie o Servidor**
+    ```sh
+    npm start
+# modelagem-back
---
+++ alerta-main/src/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+const accountSid = "AC7d2c7b80d00db698087241f0fcae0920";
+const authToken ="d2159d25664fea76db169ab1a0398869";
+const client = require('twilio')(accountSid, authToken);
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({teste:"TESTE"})
+});
+
+//rota para envio de SMS
+app.get('/sendSMS', async (req, res) => {
+  client.messages
+  .create({
+     body: 'Olá, alerta de enchente',
+     from: '+18026365560',
+     to: '+5548991951524'
+   })
+  .then(message => console.log(message.sid));
+});
+
+
+app.get('/data', async(req, res) => {
+
+    //implemente sua solução aqui
+    const thingSpeakChannelId = '2572799';
+    const thingSpeakApiKey = 'EODTIHIUB9KBEFYI';
+
+    // Number of results to retrieve
+    const numResults = 100;
+
+    // Make a GET request to ThingSpeak to fetch the latest data
+    const response = await axios.get(
+      `https://api.thingspeak.com/channels/2572799/feeds.json?results=100`,
+      {
+        headers: {
+          Authorization: `Bearer ${thingSpeakApiKey}`,
+        },
+      }
+    );
+
+    const data = response.data;
+    const results = [];
+
+    // Extract the relevant data from the ThingSpeak response
+    for (let d of data.feeds) {
+      results.push({
+        created_at: d.created_at,
+        value: d.field1,
+      });
+    }
+
+    // Send the results as JSON response
+    res.status(200).send(results)
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
